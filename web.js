a1427961b742dfb832af9b46d575ebc0805d5b44
walter wang <wfw2006@gmail.com>
2013-07-11 00:38:29 -0700

    Add a new file index.htnl

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.sendfile(_dirname + "/index.html");
+  response.sendfile(__dirname + "/index.html");
 });
 
 var port = process.env.PORT || 5000;
