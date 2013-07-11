b84382dbaac63822c4dbd2eef11628aab391777d
walter wang <wfw2006@gmail.com>
2013-07-11 00:31:04 -0700

    Add a new file index.htnl

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
+
 var express = require('express');
 var fs = require('fs');
-var bf = new buffer(2000);
+var bf = new buffer(256);
 var app = express.createServer(express.logger());
-bf = fs.readFileSync("index.html");
+bf = fs.readFileSync('./index.html');
 app.get('/', function(request, response) {
   response.send( bf.toString());
 });
